a13fee09c45c880dd5b6fe41c01ae3393616a17d
JosephKim <113419166+josephkimmm@users.noreply.github.com>
2025-05-15 16:23:30 +0900

    travel form

--- src/main/java/com/test/foodtrip/domain/travel/controller/TestController.java
+++ src/main/java/com/test/foodtrip/domain/travel/controller/TestController.java
@@ -1,4 +1,14 @@
 package com.test.foodtrip.domain.travel.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+
+    @GetMapping("/trip/travel")
+    public String travel() {
+
+        return "course";
+    }
 }
